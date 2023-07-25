00d5b335372d76373c7368b4a1fe2bd1a9358c85
Zer0h1nder <lucas.mair@web.de>
2023-07-25 20:34:50 +0200

    Use DeclareLaunchArgument within launch file itself as command line argument

--- Offboard_Control/offboard_control.launch.py
+++ Offboard_Control/offboard_control.launch.py
@@ -11,18 +11,26 @@ micro_ros_agent = ExecuteProcess(
 
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction
 
 
-def generate_launch_description():
+def launch_setup(context, *args, **kwargs):
+    # number of nodes to start
+    # good reference here: https://answers.ros.org/question/376816/how-to-pass-launch-args-dynamically-during-launch-time/ <-- argv hack should work as well
+    # and https://answers.ros.org/question/322874/ros2-what-is-different-between-declarelaunchargument-and-launchconfiguration/
+    # and https://answers.ros.org/question/396345/ros2-launch-file-how-to-convert-launchargument-to-string/ <-- chives_onion answer
+    
+    count = int(context.perform_substitution(LaunchConfiguration('count')))
+    
+    launch_array =  []
+    
+    print('The drone count is ' + str(count))
 
-    #number of nodes to start
-    count = 3
-    offboard_control =  []
-
+    # if arguments contains only the "" empty string, then it is completely left out and the next argument "--ros-args" replaces its position in argv[1]
     if count < 2:
         name_prefix = ""
-        offboard_control.append(
+        launch_array.append(
             Node(
                 package='px4_ros_com',
                 executable='offboard_control',
@@ -34,7 +42,7 @@ def generate_launch_description():
     else:
         for x in range(0, count):
             name_prefix = 'vhcl'+ str(x) +'/'
-            offboard_control.append(
+            launch_array.append(
                 Node(
                     package='px4_ros_com',
                     executable='offboard_control',
@@ -43,9 +51,11 @@ def generate_launch_description():
                     arguments=[name_prefix]
                 )
             )
-    
+    return launch_array
 
+# declare command line argument via appending count:=NUMBER when starting the launch file from terminal
+def generate_launch_description():
     return LaunchDescription(
         #micro_ros_agent,
-        offboard_control
+        [DeclareLaunchArgument('count', default_value='3')] + [OpaqueFunction(function=launch_setup)]
     )
