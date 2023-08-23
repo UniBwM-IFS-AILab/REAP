cb480f4ff5d089427d84bac5b40a6fe77a3ecbcf
Zer0h1nder <lucas.mair@web.de>
2023-08-23 15:28:23 +0200

    Update wsl_static_ip.ps1

--- wsl_static_ip.ps1
+++ wsl_static_ip.ps1
@@ -1,4 +1,10 @@
-echo "setting static vEthernet (WSL) ip address..."
+# normal echo won't work
+# echo "setting static vEthernet (WSL) ip address..."
+
+# echo wont work as it is a background task, but you could redirect to file and output file content on first shell via bash script
+# https://superuser.com/questions/1717816/how-to-run-command-when-starting-a-machine-in-wsl2
+# put following line into [boot] /etc/wsl.conf before semicolon;
+# echo "adding 172.17.208.1 as wsl ip" > /usr/init.log
 
 netsh interface ip add address "vEthernet (WSL)" 172.17.208.1 255.255.255.0
 
