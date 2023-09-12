ed781b66843bfdbc1df5417801067f6cea955e4f
Zer0h1nder <lucas.mair@web.de>
2023-09-12 16:13:29 +0200

    add home position listener

--- Offboard_Control/vehicle_global_position_listener_lib.cpp
+++ Offboard_Control/vehicle_global_position_listener_lib.cpp
@@ -41,6 +41,7 @@
 #include <rclcpp/rclcpp.hpp>
 #include <px4_msgs/msg/vehicle_global_position.hpp>
 #include <px4_msgs/msg/vehicle_odometry.hpp>
+#include <px4_msgs/msg/home_position.hpp>
 
 /**
  * @brief Vehicle global position uORB topic data callback
@@ -51,14 +52,19 @@ public:
 
 	px4_msgs::msg::VehicleGlobalPosition::SharedPtr recent_gps_msg;
 	px4_msgs::msg::VehicleOdometry::SharedPtr recent_ned_msg;
+	px4_msgs::msg::HomePosition::SharedPtr recent_home_msg;
 	
 	// name_prefix should have the format "<identifier>/"
 	explicit VehicleGlobalPositionListener(std::string name_prefix = "") : Node(name_prefix.substr(0, name_prefix.size() - 1)+ "_" + "vehicle_global_position_listener") {
 		
 		px4_msgs::msg::VehicleGlobalPosition empty_gps_msg{};
 		recent_gps_msg = std::make_shared<px4_msgs::msg::VehicleGlobalPosition>(std::move(empty_gps_msg));
+		
 		px4_msgs::msg::VehicleOdometry empty_ned_msg{};
 		recent_ned_msg = std::make_shared<px4_msgs::msg::VehicleOdometry>(std::move(empty_ned_msg));
+		
+		px4_msgs::msg::HomePosition empty_home_msg{};
+		recent_home_msg = std::make_shared<px4_msgs::msg::HomePosition>(std::move(empty_home_msg));
 
 		rmw_qos_profile_t qos_profile = rmw_qos_profile_sensor_data;
 		auto qos = rclcpp::QoS(rclcpp::QoSInitialization(qos_profile.history, 5), qos_profile);
@@ -102,11 +108,30 @@ public:
 			//std::cout << "recent_ned_msg timestamp (should be same as above): " << recent_ned_msg->timestamp << std::endl;
 			
 		});
+		
+		std::cout << "Setup home position..." << std::endl;
+		subscription_home = this->create_subscription<px4_msgs::msg::HomePosition>(
+			name_prefix + "fmu/out/home_position", qos,
+			[this](const px4_msgs::msg::HomePosition::UniquePtr msg) {
+			/*
+			float64 lat				# Latitude in degrees
+			float64 lon				# Longitude in degrees
+			float32 alt				# Altitude in meters (AMSL)
+
+			float32 x				# X coordinate in meters
+			float32 y				# Y coordinate in meters
+			float32 z				# Z coordinate in meters
+			*/
+			
+			recent_home_msg = std::make_unique<px4_msgs::msg::HomePosition>(*msg);
+			
+		});
 	}
 
 private:
 	rclcpp::Subscription<px4_msgs::msg::VehicleGlobalPosition>::SharedPtr subscription_gps;
 	rclcpp::Subscription<px4_msgs::msg::VehicleOdometry>::SharedPtr subscription_ned;
+	rclcpp::Subscription<px4_msgs::msg::HomePosition>::SharedPtr subscription_home;
 };
 
 /*
