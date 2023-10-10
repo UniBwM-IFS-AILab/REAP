8ee1697e8b7316ad4372d07e270f123e1dc0629c
Zer0h1nder <lucas.mair@web.de>
2023-10-10 14:58:20 +0200

    Update start_upf_simulation.sh

--- startup_scripts/start_upf_simulation.sh
+++ startup_scripts/start_upf_simulation.sh
@@ -21,5 +21,5 @@ wsl_instance_name="Dev_Companion"
 
 
 pushd /mnt/c
-$win_path/cmd.exe /c $win_path2/wt.exe -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_px4' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_rtps_agent' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_offboard_control' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_upf4ros' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_plan_executor'
+$win_path/cmd.exe /c $win_path2/wt.exe -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_px4' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_rtps_agent' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_offboard_control count:=1' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_upf4ros' \; new-tab -p $wsl_profile_name wsl -d $wsl_instance_name -e bash -li -c 'start_plan_executor'
 popd
