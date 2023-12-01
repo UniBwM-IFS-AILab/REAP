b736e8a52e73fea93cf00e39fc60c717c0c7a3fb
Zer0h1nder <lucas.mair@web.de>
2023-12-01 16:33:42 +0100

    Update wsl_static_ip.ps1

--- wsl_static_ip.ps1
+++ wsl_static_ip.ps1
@@ -7,9 +7,11 @@
 # echo "adding 172.17.208.1 as wsl ip" > /usr/init.log
 
 netsh interface ip add address "vEthernet (WSL)" 172.17.208.1 255.255.255.0
+netsh interface ip add address "vEthernet (WSL (Hyper-V firewall))" 172.17.208.1 255.255.255.0
 
 # to remove ip for testing purposes
 # netsh interface ip delete address "vEthernet (WSL)" 172.17.208.1 255.255.255.0
+# netsh interface ip add address "vEthernet (WSL (Hyper-V firewall))" 172.17.208.1 255.255.255.0
 
 # start from within wsl
 # /mnt/c/Windows/System32/WindowsPowerShell/v1.0/powershell.exe <path to powershell script>/wsl_static_ip.ps1
